a2ee8babad6ea831389ba893a0d7428f18f5ebfa
knogu <kotaronoguchi61@gmail.com>
2021-08-08 13:40:47 +0900

    db接続

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"github.com/gorilla/mux"
 	"fmt"
+	"log"
 
 	"meal_api/handler"
 	"gorm.io/driver/mysql"
@@ -14,7 +15,9 @@ func DBinit() {
 	dsn := "meal:password@tcp(db:3306)/meal?charset=utf8&parseTime=True&loc=Local"
 	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		fmt.Println("db connection failed")
+		log.Fatal(err)
+	} else {
+		fmt.Println("db connection succeeded")
 	}
 }
 
